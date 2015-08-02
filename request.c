0901d8a2ce8149a0e96fd1891b5272383fea4b18
sora-chan <toshinon@cock.li>
2015-08-02 16:24:48 +1000

    request.c: fixed file upload response to print https, not http.

--- request.c
+++ request.c
@@ -121,7 +121,7 @@ void *process_request(void *p)
 
 		printf("\033[1m%s, (request):\033[0m %s: File of %zu bytes uploaded (%llx)\n", strtime, cc->str_addr, r.len, id);
 
-		snprintf(buf, 128, "http://%s:%i/%llx\n", config->domainname, config->port, id);
+		snprintf(buf, 128, "https://%s:%i/%llx\n", config->domainname, config->port, id);
 		socket_puts(cc, buf);
 	}else if (r.type == R_GET){
 		char *data = 0;
